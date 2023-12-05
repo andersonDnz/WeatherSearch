083b64171cbc1ccc3df7f8e6d739cae2044ffa13
anderson <andersondinizep@gmail.com>
2023-12-04 22:30:00 -0300

    Refactor: Tipando o componente darkMode

--- app/components/darkMode/index.tsx
+++ app/components/darkMode/index.tsx
@@ -1,21 +1,25 @@
+import {FC} from 'react';
 import { FiSun, FiMoon } from "react-icons/fi";
 
+interface DarkModeToggleProps {
+    isDarkMode: boolean;
+    toggleDarkMode: () => void;
+}
 
 
-import React from 'react';
 
-const DarkModeToggle = ({ isDarkMode, toggleDarkMode }) => {
+const DarkModeToggle: FC<DarkModeToggleProps> = ({ isDarkMode, toggleDarkMode }) => {
     return (
         <div className="bg-white w-20 rounded-full">
 
-        <div
-            className={`flex items-center justify-center text-white w-10 h-10 rounded-full bg-blue-500  dark-mode-toggle cursor-pointer p-2 transition-transform duration-slow transform ${isDarkMode ? 'translate-x-full' : 'translate-x-1/3'
-                }`}
-            onClick={toggleDarkMode}
-        >
+            <div
+                className={`flex items-center justify-center text-white w-10 h-10 rounded-full bg-blue-500  dark-mode-toggle cursor-pointer p-2 transition-transform duration-slow transform ${isDarkMode ? 'translate-x-full' : 'translate-x-1/3'
+                    }`}
+                onClick={toggleDarkMode}
+            >
 
-            {isDarkMode ? (<FiMoon />) : (<FiSun />)}
-        </div>
+                {isDarkMode ? (<FiMoon />) : (<FiSun />)}
+            </div>
         </div>
 
 
